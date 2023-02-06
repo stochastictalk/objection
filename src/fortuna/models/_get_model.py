e8a1c1573c3a84e23f071c7d31fc59db36b99be5
Kaplan <kaplan@Jerome-Wynne.local>
2023-02-06 19:43:18 +0000

    Restructure PyTorch tutorial into a package.

---
+++ src/fortuna/models/_get_model.py
@@ -0,0 +1,33 @@
+import torchvision
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
+
+
+def get_model(num_classes: int) \
+    -> torchvision.models.detection.mask_rcnn.MaskRCNN:
+    """
+    num_classes includes background class e.g. PennFudan has
+    two classes - background and person.
+    
+    model.__call__ 
+    * List[FloatTensor] -> returns List[Dict] where each Dict has keys "boxes", "labels", "scores".
+    * FloatTensors can be different sizes, must be three channels.
+    * Gradients are attached to "boxes", "labels", "scores" tensors.
+    
+    """
+    # Load an instance segmentation model pre-trained on COCO.
+    model = torchvision.models.detection.maskrcnn_resnet50_fpn(weights="DEFAULT")
+
+    # Replace the pre-trained box predictor head with a new one.
+    in_features = model.roi_heads.box_predictor.cls_score.in_features    
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    # Replace the pre-trained mask predictor head with a new one.
+    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
+    model.roi_heads.mask_predictor = MaskRCNNPredictor(
+        in_channels = in_features_mask, # Number of input channels.
+        dim_reduced = 256, # Number of hidden layer units.
+        num_classes = num_classes # Number of object classes.
+    )
+
+    return model
